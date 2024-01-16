44ed1283427a2d38b26a01d3e756e2e636b2dca7
AdarshShandilya1 <shandilyaadarsh1234567@gmail.com>
2024-01-16 13:22:59 +0530

    made every thing functional

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
-import SearchBox from '@/components/SearchBox'
-import Image from 'next/image'
-import data from "@/database/data.json"
+import SearchBox from "@/components/SearchBox";
+import Image from "next/image";
+import data from "@/database/data.json";
 
 export default function Home() {
-  const skillsArray = data.skills
+  const skillsArray = data.skills;
   return (
-    <div className='flex h-screen justify-center items-center'>
-      <SearchBox
-        skillsArray = {skillsArray}
-      />
+    <div className="flex items-center justify-center h-screen  bg-cover bg-no-repeat bg-center bg-[url(https://images.pexels.com/photos/414659/pexels-photo-414659.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)]">
+      <SearchBox skillsArray={skillsArray} />
     </div>
-  )
+  );
 }
--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -5,25 +5,31 @@ import { useState } from "react";
 export default function SearchBox(props) {
   const [isVisible, setIsVisible] = useState(false);
   const [skills, setSkills] = useState(props.skillsArray);
-  const [searchSkills, setSearchSkills] = useState([]);
+  const [searchSkills, setSearchSkills] = useState(props.skillsArray);
   const [selectedSkills, setSelectedSkills] = useState([]);
   const [input, setInput] = useState("");
 
   const moveToSelected = (skill) => {
     setSelectedSkills((prev) => [...prev, skill]);
     const arr = skills.filter((s) => s != skill);
+    const arr1 = searchSkills.filter((s) => s != skill);
+    setSearchSkills(arr1);
     setSkills(arr);
     // console.log(selectedSkills);
   };
-const removeSelected = (skill) => {
-    setSkills((prev) => [...prev, skill])
-    const arr = selectedSkills.filter((s) => s != skill)
+  const removeSelected = (skill) => {
+    setSkills((prev) => [...prev, skill]);
+    const arr = selectedSkills.filter((s) => s != skill);
+    setSearchSkills((prev) => [...prev, skill]);
     setSelectedSkills(arr);
-}
+  };
   const handleInputChange = (e) => {
     e.preventDefault();
     setInput(e.target.value);
-    console.log(input);
+    const arr = skills.filter((skill) =>
+      skill.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setSearchSkills(arr);
   };
 
   const openDropdown = () => {
@@ -33,58 +39,61 @@ const removeSelected = (skill) => {
     setIsVisible(false);
   };
   return (
-    <div className=" overflow-hidden p-4 h-[500px] w-8/12 [box-shadow:rgba(100,_100,_111,_0.2)_0px_7px_29px_0px] bg-opacity-90 rounded-2xlbackdrop-filter backdrop-blur-[13px] backdrop-saturate-[157%] bg-[rgba(255,_255,_255,_0.31)] rounded-[12px] border-[1px] border-[solid] border-[rgba(209,213,219,0.3)]">
-      <label htmlFor="" className=" text-lg ">
+    <div className=" overflow-hidden p-6 h-[500px] w-6/12 shadow-[rgba(100,_100,_111,_0.2)_0px_7px_29px_0px] bg-opacity-90 rounded-2xlbackdrop-filter backdrop-blur-[8px] backdrop-saturate-[157%] bg-[rgba(255,_255,_255,_0.31)] rounded-[12px] border-[1px] border-[solid] border-[rgba(209,213,219,0.3)]">
+      <label htmlFor="" className="text-[14px] text-[#565656]">
         Choose your skills
       </label>
-      <div className="flex mt-4 rounded-md p-2 border-2 border-black">
-        <div className=" flex gap-2 w-fit overflow-scroll">
-          {selectedSkills.map((skill) => {
-            return (
-              <div  className="bg-blue-800 w-fit text-white px-4 py-2 flex gap-1 justify-between items-center rounded-full cursor-pointer">
-                <p className=" text-nowrap">{skill}</p>
-                <svg
-                    onClick={() => {removeSelected(skill)}}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6 18 18 6M6 6l12 12"
-                  />
-                </svg>
-              </div>
-            );
-          })}
-        </div>
+      <div className="flex flex-wrap gap-2 p-2 border-2 border-[#6e6e6e] rounded-md">
+        {/* <div className="flex gap-2 overflow-x-scroll w-fit hide"> */}
+        {selectedSkills.map((skill) => {
+          return (
+            <div className="flex items-center justify-between gap-1 px-4 py-2 text-white bg-[#565656] rounded-full cursor-pointer bg- w-fit">
+              <p className=" text-nowrap">{skill}</p>
+              <svg
+                onClick={() => {
+                  removeSelected(skill);
+                }}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            </div>
+          );
+        })}
+        {/* </div> */}
         <input
           onChange={handleInputChange}
           value={input}
           onFocus={openDropdown}
           // onBlur={closeDropdown}
-          className="border-none outline-none rounded-md w-full p-2  "
+          className="p-2 bg-transparent border-none rounded-md outline-none"
           type="text"
         ></input>
       </div>
       <div
         className={
           isVisible
-            ? "mt-4 h-[350px] overflow-y-scroll block"
+            ? "max-h-[350px] overflow-y-scroll flex flex-wrap  gap-x-4 gap-y-2"
             : " hidden mt-4 h-[350px] overflow-y-scroll"
         }
       >
-        {skills.map((skill) => {
+        { 
+          searchSkills.sort().map((skill) => {
           return (
             <p
               onClick={() => {
                 moveToSelected(skill);
               }}
-              className="bg-black w-fit text-white px-4 py-2 rounded-full mt-4 cursor-pointer"
+              className="px-4 py-2 mt-4 text-[white] font-semibold bg-[#5e5e5e50] opacity-100 border-[3px] border-[white] border-opacity-100 rounded-full cursor-pointer  w-fit h-fit"
             >
               {skill}
             </p>
